11ecb60a8273ac6b57f55bd7de067d6daefc789f
Adam Yellen <6933682+AdamYellen@users.noreply.github.com>
2022-07-26 10:48:48 -0400

    More cleanup

--- bin/strap.ps1
+++ bin/strap.ps1
@@ -52,8 +52,7 @@ Write-Host "Configuring System..." -ForegroundColor "Yellow"
 ## Background file: C:\someDirectory\someImage.jpg
 ## File Size Limit: 256Kb
 # Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Personalization" "LockScreenImage" "C:\someDirectory\someImage.jpg"
-if ($strap_git_name -and $strap_git_email)
-{
+if ($strap_git_name -and $strap_git_email) {
     Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticecaption" "Found this computer?"
     Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticetext" "Please contact $strap_git_name at $strap_git_email."
 }
@@ -92,80 +91,67 @@ Write-Host "Configuring Windows Defender..." -ForegroundColor "Yellow"
 ### Bootstrapping Dependencies                                                #
 ###############################################################################
 
-if (-not (Check-Command -cmdname 'choco'))
-{
+# Chocolatey
+if (-not (Check-Command -cmdname 'choco')) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null
 }
-else
-{
+else {
     choco upgrade chocolatey | Out-Null
+}
+if (-not (Check-Command -cmdname 'choco')) {
+    Write-Host "!! Exiting: Can't find Chocolatey !!" -ForegroundColor "Red"
+    Exit 1
 }
 
 # Install Git
-choco install git --params "/GitOnlyOnPath /NoAutoCrlf /NoShellIntegration /SChannel /Symlinks /Editor:VisualStudioCode" -y | Out-Null
+choco upgrade git --params "/GitOnlyOnPath /NoAutoCrlf /NoShellIntegration /SChannel /Symlinks /Editor:VisualStudioCode" -y | Out-Null
 
-# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall
-# variable and importing the Chocolatey profile module.
-# Note: Using `. $PROFILE` instead *may* work, but isn't guaranteed to.
+# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall variable and importing the Chocolatey profile module.
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 
 # Setup Git configuration.
-Write-Host "Setting up Git for Windows..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-if ($strap_git_name -and !(git config --global user.name))
-{
+Write-Host "Setting up Git for Windows..." -ForegroundColor Yellow
+if (-not (Check-Command -cmdname 'git')) {
+    Write-Host "!! Exiting: Can't find git !!" -ForegroundColor "Red"
+    Exit 1
+}
+if ($strap_git_name -and !(git config --global user.name)) {
     git config --global user.name "$strap_git_name"
 }
-if ($strap_git_email -and !(git config --global user.email))
-{
+if ($strap_git_email -and !(git config --global user.email)) {
     git config --global user.email "$strap_git_email"
 }
-if ($strap_github_user -and ((git config --global github.user) -ne $strap_github_user))
-{
+if ($strap_github_user -and ((git config --global github.user) -ne $strap_github_user)) {
     git config --global github.user "$strap_github_user"
 }
 
-# Squelch git 2.x warning message when pushing
-# if (! (git config push.default))
-# {
-#   git config --global push.default simple
-# }
-
 # Setup GitHub HTTPS credentials.
-if ($strap_github_user -and $strap_github_token)
-{
+if ($strap_github_user -and $strap_github_token) {
     Write-Output "protocol=https`nhost=github.com`n`n" | git credential reject
     Write-Output "protocol=https`nhost=github.com`nusername=$strap_github_user`npassword=$strap_github_token`n`n" | git credential approve
 }
 
 # Setup dotfiles
-if ($strap_github_user)
-{
+if ($strap_github_user) {
     $DOTFILES_URL="https://github.com/$strap_github_user/dotfiles"
-    if (git ls-remote "$DOTFILES_URL")
-    {
-        Write-Host "Fetching/Updating $strap_github_user/dotfiles from GitHub:" -ForegroundColor Green
-        Write-Host "------------------------------------" -ForegroundColor Green
-        if (Test-Path "$HOME/.dotfiles")
-        {
+    if (git ls-remote "$DOTFILES_URL") {
+        Write-Host "Fetching/Updating $strap_github_user/dotfiles from GitHub..." -ForegroundColor Yellow
+        if (Test-Path "$HOME/.dotfiles") {
             Push-Location
             Set-Location "$HOME/.dotfiles"
             git pull --rebase --autostash
             Pop-Location
         }
-        else
-        {
+        else {
             git clone "$DOTFILES_URL" "$HOME/.dotfiles"
         }
     }
 
     # run_dotfile_scripts script/setup script/bootstrap
-    if (Test-Path "$HOME/.dotfiles/script/setup.ps1")
-    {
-        Write-Host "Running dotfiles/script/setup.ps1" -ForegroundColor Green
-        Write-Host "------------------------------------" -ForegroundColor Green
+    if (Test-Path "$HOME/.dotfiles/script/setup.ps1") {
+        Write-Host "Running dotfiles/script/setup.ps1..." -ForegroundColor Yellow
         & "$HOME/.dotfiles/script/setup.ps1"
     }
 }
@@ -173,53 +159,13 @@ if ($strap_github_user)
 ###############################################################################
 ### Updates                                                                   #
 ###############################################################################
-Write-Host "Checking Windows updates..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
+Write-Host "Checking Windows updates..." -ForegroundColor Yellow
 Install-Module -Name PSWindowsUpdate -Force
-if (-not $strap_ci)
-{
-    Write-Host "Installing updates... (Computer will reboot when complete...)" -ForegroundColor Green
+if (-not $strap_ci) {
+    Write-Host "Installing updates... (Computer will reboot when complete)" -ForegroundColor Red
     Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot
 }
-else
-{
-    Write-Host "Skipping updates... (Computer will reboot now)" -ForegroundColor Green
+else {
+    Write-Host "Skipping updates... (Computer will reboot now)" -ForegroundColor Red
     Restart-Computer
 }
-
-# Read-Host -Prompt "Setup is done, Windows needs to restart to continue, press [ENTER] to restart computer."
-# Restart-Computer
-
-# # gsudo
-# PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
-
-# Write-Host "Setting up dotnet for Windows..." -ForegroundColor Green
-# Write-Host "------------------------------------" -ForegroundColor Green
-# [Environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development", "Machine")
-# [Environment]::SetEnvironmentVariable("DOTNET_PRINT_TELEMETRY_MESSAGE", "false", "Machine")
-# [Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", "Machine")
-# dotnet tool install --global dotnet-ef
-# dotnet tool update --global dotnet-ef
-
-# Write-Host "Excluding repos from Windows Defender..." -ForegroundColor Green
-# Add-MpPreference -ExclusionPath "$env:USERPROFILE\source\repos"
-# Add-MpPreference -ExclusionPath "$env:USERPROFILE\.nuget"
-# Add-MpPreference -ExclusionPath "$env:USERPROFILE\.vscode"
-# Add-MpPreference -ExclusionPath "$env:USERPROFILE\.dotnet"
-# Add-MpPreference -ExclusionPath "$env:USERPROFILE\.ssh"
-# Add-MpPreference -ExclusionPath "$env:APPDATA\npm"
-
-# Write-Host "Enabling Hardware-Accelerated GPU Scheduling..." -ForegroundColor Green
-# New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\" -Name 'HwSchMode' -Value '2' -PropertyType DWORD -Force
-
-# Write-Host "Installing Github.com/microsoft/artifacts-credprovider..." -ForegroundColor Green
-# Write-Host "------------------------------------" -ForegroundColor Green
-# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/microsoft/artifacts-credprovider/master/helpers/installcredprovider.ps1'))
-
-# # -----------------------------------------------------------------------------
-# Write-Host ""
-
-# -----------------------------------------------------------------------------
-# Write-Host "------------------------------------" -ForegroundColor Green
-# Read-Host -Prompt "Setup is done, restart is needed, press [ENTER] to restart computer."
-# Restart-Computer
