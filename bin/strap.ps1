132af6a52797fffc80233a94f09e39c4d21452dc
Adam Yellen <6933682+AdamYellen@users.noreply.github.com>
2022-07-28 15:04:20 -0400

    Enable multi-stage strapping on Windows

--- bin/strap.ps1
+++ bin/strap.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 
-param($strap_git_name, $strap_git_email, $strap_github_user, $strap_github_token, $strap_mobile, $strap_ci)
+param($strap_git_name, $strap_git_email, $strap_github_user, $strap_github_token, $strap_mobile, $strap_op_uri, $strap_stage, $strap_ci)
 
 function Check-Command($cmdname) {
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
@@ -22,170 +22,195 @@ function AddToPath {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
-###############################################################################
-### Security and Identity                                                     #
-###############################################################################
-Write-Host "Configuring System..." -ForegroundColor "Yellow"
-
-# Set Computer Name
-# (Get-WmiObject Win32_ComputerSystem).Rename("COMPUTER") | Out-Null
-# $computerName = Read-Host 'Enter New Computer Name'
-# Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
-# Rename-Computer -NewName $computerName
-
-## Set DisplayName for my account. Use only if you are not using a Microsoft Account
-if ($strap_git_name) {
-    $myIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $user = Get-WmiObject Win32_UserAccount | Where {$_.Caption -eq $myIdentity.Name}
-    $user.FullName = "$strap_git_name"
-    $user.Put() | Out-Null
-    Remove-Variable user
-    Remove-Variable myIdentity
+function UpdateStoreApps {
+    $wmiObj = Get-WmiObject -Namespace "root\cimv2\mdm\dmmap" -Class "MDM_EnterpriseModernAppManagement_AppManagement01"
+    $wmiObj.UpdateScanMethod() | Out-Null
+    Remove-Variable wmiObj    
 }
 
-###############################################################################
-### Lock Screen                                                               #
-###############################################################################
-Write-Host "Configuring Lock Screen..." -ForegroundColor "Yellow"
+# Which stage are we in?
+if ((-Not $strap_stage) -Or ($strap_stage -Lt 2))
+{
+    ###############################################################################
+    ### Stage 1                                                                   #
+    ###############################################################################
 
-## Enable Custom Background on the Login / Lock Screen
-## Background file: C:\someDirectory\someImage.jpg
-## File Size Limit: 256Kb
-# Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Personalization" "LockScreenImage" "C:\someDirectory\someImage.jpg"
-if ($strap_git_name -and $strap_git_email) {
-    Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticecaption" "Found this computer?"
-    if($strap_mobile) {
-        Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticetext" "Please contact $strap_git_name at $strap_mobile"
+    ###############################################################################
+    ### Security and Identity                                                     #
+    ###############################################################################
+    Write-Host "Configuring System..." -ForegroundColor "Yellow"
+
+    # Tell Windows Store to update now so it *might* complete before we reboot
+    UpdateStoreApps
+
+    ## Set DisplayName for my account. Use only if you are not using a Microsoft Account
+    if ($strap_git_name) {
+        $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $user = Get-WmiObject Win32_UserAccount | Where {$_.Caption -eq $myIdentity.Name}
+        $user.FullName = "$strap_git_name"
+        $user.Put() | Out-Null
+        Remove-Variable user
+        Remove-Variable myIdentity
     }
-    else {
-        Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticetext" "Please contact $strap_git_name at $strap_git_email."
-    }
-}
 
-###############################################################################
-### Windows Update & Application Updates                                      #
-###############################################################################
-Write-Host "Configuring Windows Update..." -ForegroundColor "Yellow"
+    ###############################################################################
+    ### Lock Screen                                                               #
+    ###############################################################################
+    Write-Host "Configuring Lock Screen..." -ForegroundColor "Yellow"
 
-# Disable automatic reboot after install
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "IsExpedited" 0
-
-# Disable restart required notifications
-# Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "RestartNotificationsAllowed2" 0
-
-# Disable updates over metered connections
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "AllowAutoWindowsUpdateDownloadOverMeteredNetwork" 0
-
-# Opt-In to Microsoft Update
-# $MU = New-Object -ComObject Microsoft.Update.ServiceManager -Strict
-# $MU.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") | Out-Null
-# Remove-Variable MU
-
-###############################################################################
-### Windows Defender                                                          #
-###############################################################################
-Write-Host "Configuring Windows Defender..." -ForegroundColor "Yellow"
-
-# Disable Cloud-Based Protection: Enabled Advanced: 2, Enabled Basic: 1, Disabled: 0
-# Set-MpPreference -MAPSReporting 0
-
-# Disable automatic sample submission: Prompt: 0, Auto Send Safe: 1, Never: 2, Auto Send All: 3
-# Set-MpPreference -SubmitSamplesConsent 2
-
-###############################################################################
-### Bootstrapping Dependencies                                                #
-###############################################################################
-Write-Host "Installing dependencies..." -ForegroundColor "Yellow"
-
-# Chocolatey
-# if (-not (Check-Command -cmdname 'choco')) {
-#     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null
-# }
-# else {
-#     choco upgrade chocolatey | Out-Null
-# }
-# if (-not (Check-Command -cmdname 'choco')) {
-#     Write-Host "!! Exiting: Can't find Chocolatey !!" -ForegroundColor "Red"
-#     Exit 1
-# }
-
-# Install Scoop
-iex "& {$(irm get.scoop.sh)} -RunAsAdmin" | Out-Null
-
-# Install Git
-scoop install git | Out-Null
-git config --system credential.helper manager-core
-
-# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall variable and importing the Chocolatey profile module.
-# $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
-# Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-# refreshenv
-
-# Setup Git configuration
-Write-Host "Setting up Git for Windows..." -ForegroundColor Yellow
-if (-not (Check-Command -cmdname 'git')) {
-    Write-Host "!! Exiting: Can't find git !!" -ForegroundColor "Red"
-    Exit 1
-}
-if ($strap_git_name -and !(git config --global user.name)) {
-    git config --global user.name "$strap_git_name"
-}
-if ($strap_git_email -and !(git config --global user.email)) {
-    git config --global user.email "$strap_git_email"
-}
-if ($strap_github_user -and ((git config --global github.user) -ne $strap_github_user)) {
-    git config --global github.user "$strap_github_user"
-}
-
-# Setup GitHub HTTPS credentials
-if ($strap_github_user -and $strap_github_token) {
-    Write-Output "protocol=https`nhost=github.com`n`n" | git credential reject
-    Write-Output "protocol=https`nhost=github.com`nusername=$strap_github_user`npassword=$strap_github_token`n`n" | git credential approve
-}
-
-###############################################################################
-### Dotfiles                                                                  #
-###############################################################################
-if ($strap_github_user) {
-    # Clone/update dotfiles repo
-    $DOTFILES_URL="https://github.com/$strap_github_user/dotfiles"
-    if (git ls-remote "$DOTFILES_URL") {
-        Write-Host "Fetching/Updating $strap_github_user/dotfiles from GitHub..." -ForegroundColor Yellow
-        if (Test-Path "$HOME/.dotfiles") {
-            Push-Location
-            Set-Location "$HOME/.dotfiles"
-            git pull --rebase --autostash
-            Pop-Location
+    ## Enable Custom Background on the Login / Lock Screen
+    ## Background file: C:\someDirectory\someImage.jpg
+    ## File Size Limit: 256Kb
+    # Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\Personalization" "LockScreenImage" "C:\someDirectory\someImage.jpg"
+    if ($strap_git_name -and $strap_git_email) {
+        Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticecaption" "Found this computer?"
+        if($strap_mobile) {
+            Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticetext" "Please contact $strap_git_name at $strap_mobile"
         }
         else {
-            git clone "$DOTFILES_URL" "$HOME/.dotfiles"
+            Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" "legalnoticetext" "Please contact $strap_git_name at $strap_git_email."
         }
     }
 
-    # Run our setup script if it exists
-    if (Test-Path "$HOME/.dotfiles/script/setup.ps1") {
-        Write-Host "Running dotfiles/script/setup.ps1..." -ForegroundColor Yellow
-        & "$HOME/.dotfiles/script/setup.ps1"
+    ###############################################################################
+    ### Windows Update & Application Updates                                      #
+    ###############################################################################
+    Write-Host "Configuring Windows Update..." -ForegroundColor "Yellow"
+
+    # Disable automatic reboot after install
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "IsExpedited" 0
+
+    # Disable restart required notifications
+    # Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "RestartNotificationsAllowed2" 0
+
+    # Disable updates over metered connections
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" "AllowAutoWindowsUpdateDownloadOverMeteredNetwork" 0
+
+    # Opt-In to Microsoft Update
+    # $MU = New-Object -ComObject Microsoft.Update.ServiceManager -Strict
+    # $MU.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") | Out-Null
+    # Remove-Variable MU
+
+    ###############################################################################
+    ### Windows Defender                                                          #
+    ###############################################################################
+    Write-Host "Configuring Windows Defender..." -ForegroundColor "Yellow"
+
+    # Disable Cloud-Based Protection: Enabled Advanced: 2, Enabled Basic: 1, Disabled: 0
+    # Set-MpPreference -MAPSReporting 0
+
+    # Disable automatic sample submission: Prompt: 0, Auto Send Safe: 1, Never: 2, Auto Send All: 3
+    # Set-MpPreference -SubmitSamplesConsent 2
+
+    ###############################################################################
+    ### Bootstrapping Dependencies                                                #
+    ###############################################################################
+    Write-Host "Installing dependencies..." -ForegroundColor "Yellow"
+
+    # Install Scoop
+    iex "& {$(irm get.scoop.sh)} -RunAsAdmin" | Out-Null
+
+    # Install Git
+    scoop install git | Out-Null
+    git config --system credential.helper manager-core
+
+    # Setup Git configuration
+    Write-Host "Setting up Git for Windows..." -ForegroundColor Yellow
+    if (-not (Check-Command -cmdname 'git')) {
+        Write-Host "!! Exiting: Can't find git !!" -ForegroundColor "Red"
+        Exit 1
+    }
+    if ($strap_git_name -and !(git config --global user.name)) {
+        git config --global user.name "$strap_git_name"
+    }
+    if ($strap_git_email -and !(git config --global user.email)) {
+        git config --global user.email "$strap_git_email"
+    }
+    if ($strap_github_user -and ((git config --global github.user) -ne $strap_github_user)) {
+        git config --global github.user "$strap_github_user"
+    }
+
+    # Setup GitHub HTTPS credentials
+    if ($strap_github_user -and $strap_github_token) {
+        Write-Output "protocol=https`nhost=github.com`n`n" | git credential reject
+        Write-Output "protocol=https`nhost=github.com`nusername=$strap_github_user`npassword=$strap_github_token`n`n" | git credential approve
+    }
+
+    ###############################################################################
+    ### Dotfiles                                                                  #
+    ###############################################################################
+    if ($strap_github_user) {
+        # Clone/update dotfiles repo
+        $DOTFILES_URL="https://github.com/$strap_github_user/dotfiles"
+        if (git ls-remote "$DOTFILES_URL") {
+            Write-Host "Fetching/Updating $strap_github_user/dotfiles from GitHub..." -ForegroundColor Yellow
+            if (Test-Path "$HOME/.dotfiles") {
+                Push-Location
+                Set-Location "$HOME/.dotfiles"
+                git pull --rebase --autostash
+                Pop-Location
+            }
+            else {
+                git clone "$DOTFILES_URL" "$HOME/.dotfiles"
+            }
+        }
+
+        # Run our setup script if it exists
+        if (Test-Path "$HOME/.dotfiles/script/setup.ps1") {
+            Write-Host "Running dotfiles/script/setup.ps1..." -ForegroundColor Yellow
+            & "$HOME/.dotfiles/script/setup.ps1"
+        }
+    }
+
+    ###############################################################################
+    ### Setup stage 2 for next reboot                                             #
+    ###############################################################################
+    Write-Host "Setup stage 2 for next reboot..." -ForegroundColor Yellow
+
+    # Setup a scheduled task that will run on next logon to run strap again with the same args plus -strap_stage 2
+    $stage2CLI = ""
+    $stage2CLI += $myinvocation.Line + ' -strap_stage 2'
+    schtasks /create /tn "StrapStage2" /sc onlogon /delay 0000:15 /rl highest /it /tr "powershell.exe -NoExit -File '$stage2CLI'"    <# Action when all if and elseif conditions are false #>
+
+    ###############################################################################
+    ### Updates                                                                   #
+    ###############################################################################
+    Write-Host "Checking for Windows updates..." -ForegroundColor Yellow
+
+    # Give the MS Store one more chance to update
+    UpdateStoreApps
+
+    # Windows updates
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+    Install-Module -Name PSWindowsUpdate -Force
+    if (-not $strap_ci) {
+        Write-Host "Installing updates... (Computer will reboot when complete)" -ForegroundColor Red
+        Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot | Out-Null
+    }
+    else {
+        Write-Host "Skipping updates... (Computer will reboot now)" -ForegroundColor Red
+        Restart-Computer
     }
 }
-
-###############################################################################
-### Updates                                                                   #
-###############################################################################
-Write-Host "Checking for Windows updates..." -ForegroundColor Yellow
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
-Install-Module -Name PSWindowsUpdate -Force
-
-# Tell Windows Store to update (no way to wait for it to complete *sigh*)
-$wmiObj = Get-WmiObject -Namespace "root\cimv2\mdm\dmmap" -Class "MDM_EnterpriseModernAppManagement_AppManagement01"
-$wmiObj.UpdateScanMethod() | Out-Null
-Remove-Variable wmiObj
-
-if (-not $strap_ci) {
-    Write-Host "Installing updates... (Computer will reboot when complete)" -ForegroundColor Red
-    Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot | Out-Null
-}
 else {
-    Write-Host "Skipping updates... (Computer will reboot now)" -ForegroundColor Red
-    Restart-Computer
+    ###############################################################################
+    ### Stage 2                                                                   #
+    ###############################################################################
+    Write-Host "Continue strapping..." -ForegroundColor "Yellow"
+
+    # Cleanup our scheduled task
+    schtasks /delete /tn "StrapStage2"
+
+    # Run the strap-after-setup scripts
+    if (Test-Path "$HOME/.dotfiles/script/strap-after-setup.ps1") {
+        Write-Host "Running dotfiles/script/strap-after-setup.ps1..." -ForegroundColor Yellow
+        if ($strap_op_uri) {
+            & "$HOME/.dotfiles/script/strap-after-setup.ps1 -strap_op_uri '$strap_op_uri'"
+        }
+        else {
+            & "$HOME/.dotfiles/script/strap-after-setup.ps1"
+        }
+    }
+
+    Write-Host "Your system is now Strap'd!" -ForegroundColor "Green"
 }
